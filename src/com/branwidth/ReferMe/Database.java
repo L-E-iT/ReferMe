eac76b3186cd0f6def9ac49fb57e9fc7183c685e
L-E-it <ellio2bj@dukes.jmu.edu>
2017-11-04 16:28:13 -0400

    Database Methods complete
    Completed Database Methods for all queries for statements.

--- src/com/branwidth/ReferMe/Database.java
+++ src/com/branwidth/ReferMe/Database.java
@@ -2,12 +2,8 @@ package com.branwidth.ReferMe;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
+import java.util.*;
 
 
 public class Database {
@@ -72,39 +68,187 @@ public class Database {
         }
     }
 
-    public void setIsReferred() {
+    /*
+    * Table Reference:
+    * Table: playersReferred
+    * Columns: uuid, isReferred, referredBy
+    *
+    * Table: playersData
+    * Columns: uuid, playersReferred
+    * */
 
+    /*
+    * Set if a user is Referred
+    *
+    * @param uuid
+    * */
+    public static void setIsReferred(UUID uuid) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("UPDATE playersReferred SET isReferred = ? WHERE uuid = ?");
+        pstmt.setInt(1, 1);
+        pstmt.setString(2, uuid.toString());
+        pstmt.execute();
     }
 
-    public boolean getIsReferred() {
-        return true;
+    /*
+    * Set if a user is not Referred
+    *
+    * @param uuid
+    * */
+    public static void setIsNotReferred(UUID uuid) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("UPDATE playersReferred SET isReferred = ? WHERE uuid = ?");
+        pstmt.setInt(1, 0);
+        pstmt.setString(2, uuid.toString());
+        pstmt.execute();
     }
 
-    public void createUser() {
+    /*
+    * Get if a user is Referred
+    *
+    * @param uuid
+    * */
+    public static boolean getIsReferred(UUID uuid) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("SELECT isReferred FROM playersReferred WHERE uuid = ?");
+        pstmt.setString(1, uuid.toString());
 
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            if (rs.getInt("isReferred") == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 
-    public void setReferredBy() {
+    /*
+    * Get if a user exists
+    *
+    * @param uuid
+    * */
+    public static boolean isUser(UUID uuid) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM playersReferred WHERE uuid = ?");
+        pstmt.setString(1, uuid.toString());
 
+        if (pstmt.executeQuery().next()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean getReferredBy() {
-        return true;
+    /*
+    * Create a user, set isReferred to 0, no Referrer
+    *
+    * @param uuid
+    * */
+    public static void createUser(UUID uuid) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("INSERT INTO playersReferred(uuid, isReferred, referredBy) VALUES(?,?,?)");
+        pstmt.setString(1, uuid.toString());
+        pstmt.setInt(2, 0);
+        pstmt.setString(3, null);
+
+        pstmt.execute();
     }
 
-    public int getPlayersReferred() {
+    /*
+    * Set who a user is ReferredBy
+    *
+    * @param uuidReferred
+    * @param uuidReferrer
+    * */
+    public static void setReferredBy(UUID uuidReferred, UUID uuidReferrer) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("UPDATE playersReferred SET referredBy = ? WHERE uuid = ?");
+        pstmt.setString(1, uuidReferrer.toString());
+        pstmt.setString(2, uuidReferred.toString());
+
+        pstmt.execute();
+    }
+
+    /*
+    * Get who a user is Referred by
+    *
+    * @param uuid
+    * */
+    public static String getReferredBy(UUID uuid) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("SELECT referredBy FROM playersReferred WHERE uuid = ?");
+        pstmt.setString(1, uuid.toString());
+
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            if (!rs.getString("referredBy").equals(null)) {
+                return rs.getString("referredBy");
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    /*
+    * Get the amount of users a player has referred
+    *
+    * @param uuid
+    * */
+    public static int getPlayersReferred(UUID uuid) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("SELECT playersReferred FROM playersData WHERE uuid = ?");
+        pstmt.setString(1, uuid.toString());
+
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            return rs.getInt("playersReferred");
+        }
         return 0;
     }
 
-    public void addToPlayersReferred() {
+    /*
+    * Add 1 to the amount a user has referred
+    *
+    * @param uuid
+    * */
+    public static void addToPlayersReferred(UUID uuid) throws SQLException {
+        int currentReferredCount = getPlayersReferred(uuid);
+        PreparedStatement pstmt = conn.prepareStatement("UPDATE playersData SET playersReferred = ? WHERE uuid = ?");
 
+        pstmt.setInt(1, ++currentReferredCount);
+        pstmt.setString(2, uuid.toString());
+
+        pstmt.execute();
     }
 
-    public void removeFromPlayersReferred() {
+    /*
+    * subtract 1 to the amount a user has referred
+    *
+    * @param uuid
+    * */
+    public static void removeFromPlayersReferred(UUID uuid) throws SQLException {
+        int currentReferredCount = getPlayersReferred(uuid);
+        PreparedStatement pstmt = conn.prepareStatement("UPDATE playersData SET playersReferred = ? WHERE uuid = ?");
 
+        pstmt.setInt(1, --currentReferredCount);
+        pstmt.setString(2, uuid.toString());
+
+        pstmt.execute();
     }
 
-    public ArrayList<String> getTopReferrers() {
-        return new ArrayList<>();
+    /*
+    * Get a list of the top Referrers, specified amount in list by int
+    *
+    * @param count
+    * */
+    public static Map<String, Integer> getTopReferrers(int count) throws SQLException {
+        Map<String, Integer> playerList = new HashMap<>();
+        int i = 0;
+        PreparedStatement pstmt = conn.prepareStatement("SELECT playersReferred, uuid FROM playersData ORDER BY playersReferred DESC");
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next())
+            if (i < count) {
+                playerList.put(rs.getString("uuid"), rs.getInt("playersReferred"));
+                i++;
+            } else {
+                break;
+            }
+        return playerList;
     }
 }
